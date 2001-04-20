e2298750ca90f8f18066a0c84ec5f2c4ec0c6d10
evlad <ev1ad@yandex.ru>
2001-04-20 17:57:12 +0000

    + dwtest2.cpp for closed loop control systems.

--- dtf/dwtest2.cpp
+++ dtf/dwtest2.cpp
@@ -1,5 +1,5 @@
-/* dwtest.cpp */
-static char rcsid[] = "$Id: dwtest.cpp,v 1.3 2001-04-20 17:57:12 vlad Exp $";
+/* dwtest2.cpp */
+static char rcsid[] = "$Id: dwtest2.cpp,v 1.1 2001-04-20 17:57:12 vlad Exp $";
 
 #include <math.h>
 #include <stdio.h>
@@ -17,38 +17,43 @@ static char rcsid[] = "$Id: dwtest.cpp,v 1.3 2001-04-20 17:57:12 vlad Exp $";
 
 
 /***********************************************************************
- * Gets two time series: S(i) and N(i).  Creates O(i)=DTF(S(i)+N(i))
- * and computes MSE of O(i)-S(i).
+ * Gets two time series: S(i) and N(i).  Creates
+ * O(i)=DTF1*DTF2(S(i)+N(i)) and computes MSE of O(i)-S(i).
  ***********************************************************************/
 main (int argc, char* argv[])
 {
   if(argc != 5 && argc != 6)
     {
-      fprintf(stderr, "Error: need 4 or 5 arguments\n");
-      printf("Usage: dwtest DiscrTrFunc SignalSeries NoiseSeries"\
+      fprintf(stderr, "Error: need 5 or 6 arguments\n");
+      printf("Usage: dwtest2 ContrTrFunc PlantTrFunc SignalSeries NoiseSeries"\
 	     " OutSeries [InSeries]\n");
       return 1;
     }
 
-  char	*dtf_file = argv[1];
-  char	*signal_file = argv[2];
-  char	*noise_file = argv[3];
-  char	*out_file = argv[4];
+  char	*dtf1_file = argv[1];
+  char	*dtf2_file = argv[2];
+  char	*signal_file = argv[3];
+  char	*noise_file = argv[4];
+  char	*out_file = argv[5];
   char	*in_file = NULL;
 
   if(argc == 6)
-    in_file = argv[5];
+    in_file = argv[6];
 
   NaOpenLogFile("dwtest.log");
 
   try{
     int			n;
-    NaReal		e2;
-    NaTransFunc		dtf;
-    NaConfigPart	*conf_list[] = { &dtf };
-    NaConfigFile	conf_file(";NeuCon transfer", 1, 0);
-    conf_file.AddPartitions(NaNUMBER(conf_list), conf_list);
-    conf_file.LoadFromFile(dtf_file);
+    NaReal		e2, e;
+    NaTransFunc		dtf1, dtf2;
+    NaConfigPart	*conf1_list[] = { &dtf1 };
+    NaConfigPart	*conf2_list[] = { &dtf2 };
+    NaConfigFile	conf1_file(";NeuCon transfer", 1, 0);
+    NaConfigFile	conf2_file(";NeuCon transfer", 1, 0);
+    conf1_file.AddPartitions(NaNUMBER(conf1_list), conf1_list);
+    conf2_file.AddPartitions(NaNUMBER(conf2_list), conf2_list);
+    conf1_file.LoadFromFile(dtf1_file);
+    conf2_file.LoadFromFile(dtf2_file);
 
     NaDataFile	*dfSignal = OpenInputDataFile(signal_file);
     NaDataFile	*dfNoise = OpenInputDataFile(noise_file);
@@ -60,17 +65,20 @@ main (int argc, char* argv[])
 
     dfSignal->GoStartRecord();
     dfNoise->GoStartRecord();
-    dtf.Reset();
+    dtf1.Reset();
+    dtf2.Reset();
     n = 0;
+    e = 0.;
     e2 = 0.;
     do{
       ++n;
-      NaReal	fSignal, fNoise, fIn, fOut;
+      NaReal	fSignal, fNoise, fIn, fOut, fTmp;
       fSignal = dfSignal->GetValue();
       fNoise = dfNoise->GetValue();
 
-      fIn = fSignal + fNoise;
-      dtf.Function(&fIn, &fOut);
+      fIn = fSignal + fNoise - e;
+      dtf1.Function(&fIn, &fTmp);
+      dtf2.Function(&fTmp, &fOut);
 
       if(NULL != dfIn)
 	{
@@ -81,6 +89,7 @@ main (int argc, char* argv[])
       dfOut->AppendRecord();
       dfOut->SetValue(fOut);
 
+      e = fOut;
       e2 += (fOut - fSignal) * (fOut - fSignal);
     }while(dfSignal->GoNextRecord() && dfNoise->GoNextRecord());
 
