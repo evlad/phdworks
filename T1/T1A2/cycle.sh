207c5c5630ff11537c263b4074fd53423b90f936
evlad <ev1ad@yandex.ru>
2001-12-20 21:04:40 +0000

    Fixes in cycle.sh

--- T1/T1A2/cycle.sh
+++ T1/T1A2/cycle.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for Gdelay in 6 7 8 9
+for Gdelay in 1 2 3 4 5 6 7 8 9
 do
     echo "~~~ Gdelay=0.${Gdelay} ~~~"
     rm -f _data_ok
-    ./select_u_k.sh g5_${Gdelay}.tf
+    ./select_dudy.sh g5_${Gdelay}.tf
     mkdir ${Gdelay}
     mv nnp_[1-4]+[1-4]_831 ${Gdelay}
 done
