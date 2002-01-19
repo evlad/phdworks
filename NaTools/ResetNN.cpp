ab8eda190b35df5da48395670cd6e84e298ee940
evlad <ev1ad@yandex.ru>
2002-01-19 14:27:12 +0000

    + EvalNN; fix small bug in ResetNN

--- NaTools/ResetNN.cpp
+++ NaTools/ResetNN.cpp
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
     if(argc != 2 && argc != 3)
       {
 	printf("Neural network initializer %s\n", nnfile.Magic());
-	printf("Usage: %s File.nn [NewFile.nn]\n");
+	printf("Usage: %s File.nn [NewFile.nn]\n", argv[0]);
 	return 1;
       }
 
