ae153f20ab39329628778f0d427732e2a3883678
evlad <ev1ad@yandex.ru>
2002-01-23 06:41:10 +0000

    Update dnnplant the same manner as dplantid

--- dplantid/dnnplant.cpp
+++ dplantid/dnnplant.cpp
@@ -1,5 +1,5 @@
 /* dnnplant.cpp */
-static char rcsid[] = "$Id: dnnplant.cpp,v 1.2 2002-01-11 21:21:17 vlad Exp $";
+static char rcsid[] = "$Id: dnnplant.cpp,v 1.3 2002-01-23 06:41:10 vlad Exp $";
 
 #include <math.h>
 #include <stdio.h>
@@ -48,17 +48,54 @@ main (int argc, char* argv[])
 
     // Configure nodes
     nnroe.nnplant.set_transfer_func(&au_nn);
-    nnroe.delay_u.set_delay(au_nn.descr.nInputsRepeat,
-			    au_nn.descr.InputDelays());
-    nnroe.delay_y.set_delay(au_nn.descr.nOutputsRepeat,
-			    au_nn.descr.OutputDelays());
 
-    NaPNEvent   pnev;
+    unsigned	*input_delays = au_nn.descr.InputDelays();
+    unsigned	*output_delays = au_nn.descr.OutputDelays();
+
+    nnroe.delay_u.set_delay(au_nn.descr.nInputsRepeat, input_delays);
+    nnroe.delay_y.set_delay(au_nn.descr.nOutputsRepeat, output_delays);
+
+    unsigned	iDelay_u = nnroe.delay_u.get_max_delay();
+    unsigned	iDelay_y = nnroe.delay_y.get_max_delay();
+    unsigned	iSkip_u, iSkip_y;
+    unsigned	iDelay_yt, iSkip_yt;
+
+    // Skip u or y due to absent earlier values of y or u
+    if(iDelay_y >= iDelay_u)
+      {
+	iSkip_u = iDelay_y - iDelay_u + 1;
+      }
+    else /* if(iDelay_u > iDelay_y) */
+      {
+	iSkip_y = iDelay_u - iDelay_y - 1;
+      }
+    iDelay_yt = iDelay_y + iSkip_y;
+    iSkip_yt = 1 + iDelay_yt;
+
+    nnroe.skip_u.set_skip_number(iSkip_u);
+
+    nnroe.skip_y.set_skip_number(iSkip_y);
+
+    // Additional delay for target value
+    nnroe.skip_yt.set_skip_number(iSkip_yt);
+    nnroe.delay_yt.set_delay(0);
+    nnroe.delay_yt.add_delay(iDelay_yt);
+
+    /*
+    printf("delay_u=%d,  skip_u=%d\n", iDelay_u, iSkip_u);
+    printf("delay_y=%d,  skip_y=%d\n", iDelay_y, iSkip_y);
+    printf("delay_yt=%d,  skip_yt=%d\n", iDelay_yt, iSkip_yt);
+    */
+
+    // Duplicate input y series at the same time ticks as nn_y
+    nnroe.fetch_y.set_output(0);
 
     nnroe.in_u.set_input_filename(argv[2]);
     nnroe.in_y.set_input_filename(argv[3]);
     nnroe.nn_y.set_output_filename(argv[4]);
     nnroe.tr_y.set_output_filename("/tmp/tr_y.dat");
+
+    NaPNEvent   pnev;
 
     try{
       nnroe.link_net();
