aadc93696b67f7dfe41f91c6b7b455dc1314d11a
evlad <ev1ad@yandex.ru>
2003-09-05 20:50:03 +0000

    Remove NaNNet.[h,cpp]

--- NeuArch/NeuArch.cpp
+++ NeuArch/NeuArch.cpp
@@ -1,5 +1,5 @@
 /* NeuArch.cpp */
-static char rcsid[] = "$Id: NeuArch.cpp,v 1.2 2001-05-15 06:02:23 vlad Exp $";
+static char rcsid[] = "$Id: NeuArch.cpp,v 1.3 2003-09-05 20:50:03 vlad Exp $";
 //---------------------------------------------------------------------------
 #include <condefs.h>
 #pragma hdrstop
@@ -33,7 +33,6 @@ USEUNIT("NaMath.cpp");
 USEUNIT("NaSysSch.cpp");
 USEUNIT("NaSSPlan.cpp");
 USEUNIT("NaSSCtrl.cpp");
-USEUNIT("NaNNet.cpp");
 USEUNIT("NaTrFunc.cpp");
 //---------------------------------------------------------------------------
 #define Library
