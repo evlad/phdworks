097e49388968e73b9526b602b285ebd63d44f2d0
evlad <ev1ad@yandex.ru>
2007-10-08 20:22:28 +0000

    1) include with relative path 2) fixed erf_a() implementation

--- razlad/distrib/normaldistr.hpp
+++ razlad/distrib/normaldistr.hpp
@@ -1,9 +1,9 @@
 /* normaldistr.hpp */
-/* $Id: normaldistr.hpp,v 1.2 2007-09-10 21:13:23 evlad Exp $ */
+/* $Id: normaldistr.hpp,v 1.3 2007-10-08 20:22:28 evlad Exp $ */
 #ifndef __normaldistr_hpp
 #define __normaldistr_hpp
 
-#include "numintegral.hpp"
+#include "../integrator/numintegral.hpp"
 
 
 /**
@@ -38,6 +38,10 @@ double	pdf (double x, double mx = 0.0, double sx = 1.0);
 /** Cummulative distribution function calculation: F(x)=int f(x) */
 double	cdf (double x, double mx = 0.0, double sx = 1.0, double prec = 1e-6);
 
+/** Cummulative distribution function calculation (Hill, AS66):
+    F(x)=int f(x) */
+double	cdf_as66 (double x, double mx = 0.0, double sx = 1.0);
+
 /** Error function: e(x)=2 * int_0^x(exp(-t**2)dt)/sqrt(PI) */
 double	erf (double x, double prec = 1e-6);
 
@@ -49,6 +53,10 @@ double	erfc (double x, double prec = 1e-6);
 
 /** Factorial calculation: fact(n) = n! = 1*2*3*...*(n-1)*n */
 double	fact (int n);
+
+/** AS66 implementation */
+double	alnorm  (double x, int upper = 0);
+
 
 
 #endif /* normaldistr.hpp */
