56ff2a5e9c86b14865af479b3b31fa4d05138b0b
evlad <ev1ad@yandex.ru>
2010-10-08 19:54:43 +0000

    + Lab1 menu and general menu refactoring

--- noc_labs/main.tcl
+++ noc_labs/main.tcl
@@ -2,37 +2,33 @@
 # \
 exec wish "$0" ${1+"$@"}
 
-#package require Plotchart
-
 set w ""
 #catch {destroy .}
 #toplevel $w
 wm title . "Main menu of laboratory works"
-wm iconname . "Lab menu"
+wm iconname . "Labs main menu"
 #positionWindow $w
 
 option readfile noc_labs.ad
 
-#    main   {Freesans 11}
-array set widgetFont {
-    main   "-*-helvetica-*-r-*-*-14-*-*-*-*-*-koi8-*"
-    bold   {Helvetica 12 bold}
-    title  {Helvetica 18 bold}
-    status {Helvetica 10}
-    vars   {Helvetica 14}
+source lab1.tcl
+
+set menuContent {
+  "Лабораторная работа №1" "Нейросетевая имитация линейного регулятора"
+  "Лабораторная работа №2" "Построение нейросетевой модели объекта управления"
+  "Лабораторная работа №3" "Синтез нейросетевого оптимального регулятора"
+  "Лабораторная работа №4" "Нейросетевое управление нестационарным объектом"
 }
 
-#set font $widgetFont(main)
+set i 0
+foreach {label title} $menuContent {
+  incr i
+  set text "$label\n$title"
+  pack [button $w.lab${i}_button -text "$text" \
+    -command "MenuLab$i \"$w\" \"$text\""] -fill x -side top -expand yes -pady 2
+}
 
-button $w.lab1 -text "Лабораторная работа №1\n\
-Нейросетевая имитация линейного регулятора"
-button $w.lab2 -text "Лабораторная работа №2\n\
-Построение нейросетевой модели объекта управления"
-button $w.lab3 -text "Лабораторная работа №3\n\
-Синтез нейросетевого оптимального регулятора"
-button $w.lab4 -text "Лабораторная работа №4\n\
-Нейросетевое управление нестационарным объектом"
-button $w.quit -text "Выход" -command { exit }
+button $w.quit_button -text "Выход" -command { exit }
+pack $w.quit_button -side top -expand yes -pady 2
 
-pack $w.lab1 $w.lab2 $w.lab3 $w.lab4 -fill x -side top -expand yes -pady 2
-pack $w.quit -side top -expand yes -pady 2
+# End of file
