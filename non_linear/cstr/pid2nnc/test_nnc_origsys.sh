50f154209b55e86b6fb9ff7fad945f9bfab77862
evlad <ev1ad@yandex.ru>
2011-03-15 23:12:25 +0000

    Success with NNController on e5r1

--- non_linear/cstr/pid2nnc/test_nnc_origsys.sh
+++ non_linear/cstr/pid2nnc/test_nnc_origsys.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-dcsloop origsys.par nnc_mode=e+r contr_kind=nnc nncontr=nnc_er_1.nn \
+# Mode:
+nnc="nncontr=nnc_e5r1_95_1.nn"
+
+dcsloop.new origsys.par contr_kind=nnc $nnc \
   linplant_tf=../model_proof/cstrplant.cof \
   lincontr_tf=../model_proof/pid.cof \
   in_r=r_learn.bis in_n=n_learn.bis \
   out_u=nn_u_learn.dat out_e=nn_e_learn.dat out_ny=nn_ny_learn.dat
 
-dcsloop origsys.par nnc_mode=e+r contr_kind=nnc nncontr=nnc_er_1.nn \
+dcsloop.new origsys.par contr_kind=nnc $nnc \
   linplant_tf=../model_proof/cstrplant.cof \
   lincontr_tf=../model_proof/pid.cof \
   in_r=r_test.bis in_n=n_test.bis \
