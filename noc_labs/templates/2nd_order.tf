297d5223c5914c28893d40d7d028b092f38e3226
evlad <ev1ad@yandex.ru>
2011-11-19 18:56:10 +0000

    + 3rd order and fix 1rd and 2nd order transfer functions

--- noc_labs/templates/2nd_order.tf
+++ noc_labs/templates/2nd_order.tf
@@ -4,7 +4,7 @@
 ; type:    TransferFunction
 ; label:   Звено 2-го порядка
 ; key_pos: K 0 d1 0 d2 1 d3 2 d4 5 d5 6
-; formula: K*z^2/(z^2+d1*z+d2)
+; formula: K*(d1*z^2+d2*z+d3)/(z^2+d4*z+d5)
 product 2
 polyfrac 0
 1 / 1   ; K
