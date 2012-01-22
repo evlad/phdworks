abb9b3df1a304706f5d9749ae69ff77fad3e6a41
evlad <ev1ad@yandex.ru>
2012-01-22 09:37:51 +0000

    + noc_eta_lab - copy of etalon project which fits for NNACS

--- noc_etalon/woc.tf
+++ noc_etalon/woc.tf
@@ -1,5 +1,13 @@
 ;NeuCon transfer 1.0
-; Wiener optimal controller for given plant
 [TransferFunction]
-polyfrac 0	 ; transfer function
- 0.975 -0.4875 /  1 -0.9942	 ; num/den
+; idname: 1st_order
+; type: TransferFunction
+; label: Звено 1-го порядка
+; key_pos: K 0 d1 0 d2 1 d3 4
+; formula: K*(d1*z+d2)/(z+d3)
+product 2
+polyfrac 0
+0.4875 / 1 ; K
+polyfrac 0
+2 -1 / 1 -0.9942 ; d1 d2 d3
+
